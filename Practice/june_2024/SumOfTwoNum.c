35c7c223e414f2d6bda10f72effe4238e159d791
Ravi-9431 <ravi4personal9431@gmail.com>
2024-06-08 10:34:43 +0530

    Program which add two numbers

---
+++ Practice/june_2024/SumOfTwoNum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+  int first, second, sum;
+  printf("Enter your First Number : ");
+  scanf("%d", &first);
+  printf("Enter your Second Number : ");
+  scanf("%d", &second);
+  sum = first + second;
+  printf("Your Sum is : %d\n", sum);
+
+  return 0;
+}
